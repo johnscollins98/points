191c72c8f942bee3e2d561100b50ecf2cdc9488f
John Collins <johnscollins98@gmail.com>
2024-05-07 18:14:44 +0100

    add confirmation modal when removing admin
    also make it so an admin can't remove themselves

--- src/app/_components/AdminFormClient.tsx
+++ src/app/_components/AdminFormClient.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-import { ActionIcon, Avatar, Button, CloseIcon, Select } from "@mantine/core";
+import {
+  ActionIcon,
+  Avatar,
+  Button,
+  CloseIcon,
+  Modal,
+  Select,
+} from "@mantine/core";
 import { type User } from "next-auth";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState, type FormEvent } from "react";
 import { api } from "~/trpc/react";
@@ -13,6 +21,8 @@ interface Props {
 export const AdminFormClient = ({ users }: Props) => {
   const router = useRouter();
 
+  const { data: session } = useSession();
+
   const admins = users.filter((u) => u.admin);
 
   const [admin, setAdmin] = useState<User["id"] | null>(null);
@@ -20,6 +30,7 @@ export const AdminFormClient = ({ users }: Props) => {
   const { mutate: addAdmin } = api.admin.addAdmin.useMutation({
     onSuccess: () => router.refresh(),
   });
+
   const { mutate: removeAdmin } = api.admin.removeAdmin.useMutation({
     onSuccess: () => router.refresh(),
   });
@@ -34,42 +45,77 @@ export const AdminFormClient = ({ users }: Props) => {
     }
   };
 
-  const onRemoveAdmin = (id: string) => {
-    removeAdmin(id);
+  const [toRemove, setToRemove] = useState<User | null>(null);
+
+  const onRemoveAdmin = (e: FormEvent<HTMLFormElement>, user: User | null) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (user) {
+      removeAdmin(user.id);
+      setToRemove(null);
+    }
   };
 
   return (
-    <div className="flex flex-col gap-3">
-      <h2 className="text-lg font-bold">Admins</h2>
-      <div className="flex flex-col gap-1">
-        {admins.map((admin) => (
-          <div key={admin.id} className="flex justify-between">
-            <div className="flex items-center gap-1">
-              <Avatar src={admin.image} size="sm" />
-              {admin.name}
+    <>
+      <div className="flex flex-col gap-3">
+        <h2 className="text-lg font-bold">Admins</h2>
+        <div className="flex flex-col gap-1">
+          {admins.map((admin) => (
+            <div key={admin.id} className="flex justify-between">
+              <div className="flex items-center gap-1">
+                <Avatar src={admin.image} size="sm" />
+                {admin.name}
+              </div>
+              <div>
+                {session?.user.email !== admin.email && (
+                  <ActionIcon onClick={() => setToRemove(admin)}>
+                    <CloseIcon />
+                  </ActionIcon>
+                )}
+              </div>
             </div>
-            <div>
-              <ActionIcon onClick={() => onRemoveAdmin(admin.id)}>
-                <CloseIcon />
-              </ActionIcon>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <form onSubmit={handleAddSubmit} className="flex items-center gap-1">
+          <Select
+            searchable
+            value={admin}
+            data={users
+              .filter((u) => !u.admin && (u.name ?? u.email))
+              .map((u) => ({ label: (u.name ?? u.email)!, value: u.id }))}
+            onChange={(v) => setAdmin(v)}
+            clearable
+            placeholder="Add new admin..."
+            required
+          />
+          <Button type="submit">Add Admin</Button>
+        </form>
       </div>
-      <form onSubmit={handleAddSubmit} className="flex items-center gap-1">
-        <Select
-          searchable
-          value={admin}
-          data={users
-            .filter((u) => !u.admin && (u.name ?? u.email))
-            .map((u) => ({ label: (u.name ?? u.email)!, value: u.id }))}
-          onChange={(v) => setAdmin(v)}
-          clearable
-          placeholder="Add new admin..."
-          required
-        />
-        <Button type="submit">Add Admin</Button>
-      </form>
-    </div>
+      <Modal
+        opened={toRemove !== null}
+        onClose={() => setToRemove(null)}
+        title="Remove Admin"
+        centered
+      >
+        <form
+          onSubmit={(e) => onRemoveAdmin(e, toRemove)}
+          onReset={() => setToRemove(null)}
+          className="flex flex-col gap-2"
+        >
+          Are you sure you want to remove {toRemove?.name ?? toRemove?.email} as
+          an admin?
+          <div className="flex justify-end gap-1">
+            <Button variant="default" type="reset">
+              No
+            </Button>
+            <Button color="red" type="submit">
+              Yes
+            </Button>
+          </div>
+        </form>
+      </Modal>
+    </>
   );
 };
